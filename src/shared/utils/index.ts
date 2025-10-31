4844bae171e2bc98b96ba7ef84870d7f07b8ec24
tsegay <tsegay.berhanu186@gmail.com>
2025-11-01 00:54:39 +0300

    chore(utils):re-exporting

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -3,3 +3,4 @@ export * from "./token.util.js";
 export * from "./crypto.util.js";
 export * from "./jwt.util.js";
 export * from "./api-responder.util.js"
+export * from "./parse-with-schema.util.js"
