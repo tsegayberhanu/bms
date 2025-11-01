d0d642be6184decbf37946669cd52afcd448f535
tsegay <tsegay.berhanu186@gmail.com>
2025-11-01 18:23:25 +0300

    chore(app): add end points

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,41 @@
 import express from "express";
 import helmet from "helmet";
-import hpp from "hpp"
+import hpp from "hpp";
 import type { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { notFoundHandler } from "./shared/middlewares/index.js";
 import { errorHandlerMiddleware } from "./shared/middlewares/index.js";
 import authRouter from "./modules/auth/auth.route.js";
-import { AuthRepository } from "./modules/auth/auth.repository.js";
 import { billRouter } from "./modules/bill/index.js";
 import { paymentRoutes } from "./modules/payment/payment.route.js";
 import reminderRouter from "./modules/reminder/reminder.route.js";
 import analyticsRouter from "./modules/analytics/analytics.route.js";
+import setupSwagger from "./swagger.js";
+import userRouter from "./modules/user/user.route.js";
+import { APIResponder } from "./shared/utils/api-responder.util.js";
 
 const app = express();
 app.use(helmet());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(hpp());
 
 app.use("/api/auth", authRouter);
 app.use("/api/bills", billRouter);
-app.use("/api/payments", paymentRoutes)
-app.use("/api/reminders", reminderRouter)
-app.use("/api/analytics", analyticsRouter)
+app.use("/api/payments", paymentRoutes);
+app.use("/api/reminders", reminderRouter);
+app.use("/api/analytics", analyticsRouter);
+app.use("/api/users", userRouter);
 
 app.use("/api/health", async (_req: Request, res: Response) => {
-  res.json({
-    status:"success",
-    message:"server running healthy"
-  })
+  APIResponder.ok(res, {
+    time:new Date().toISOString(),
+  });
 });
 
-app.use("/api/users", async (_req: Request, res: Response) => {
-  const users = await AuthRepository.getAllUsers()
-  res.status(200).json(users)
-});
+setupSwagger(app);
 
 app.use(notFoundHandler);
-app.use(errorHandlerMiddleware)
+app.use(errorHandlerMiddleware);
 export default app;
