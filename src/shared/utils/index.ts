b5882f3b3cd47a4367dc71799337b48fd937371b
tsegay <tsegay.berhanu186@gmail.com>
2025-11-01 13:29:46 +0300

    chore(utils): add logger

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -4,3 +4,4 @@ export * from "./crypto.util.js";
 export * from "./jwt.util.js";
 export * from "./api-responder.util.js"
 export * from "./parse-with-schema.util.js"
+export * from "./logger.util.js"
---
+++ src/shared/utils/logger.util.ts
@@ -0,0 +1,4 @@
+export const logger = {
+  info: (msg: string) => console.log(`[INFO] ${new Date().toISOString()} - ${msg}`),
+  error: (msg: string) => console.error(`[ERROR] ${new Date().toISOString()} - ${msg}`),
+};
