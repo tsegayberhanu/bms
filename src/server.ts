c51f3d25a8ccade071b3a02214ad7121621023fe
tsegay <tsegay.berhanu186@gmail.com>
2025-11-02 10:43:39 +0300

    fix(reminder-cron): import cron module in app entry to start scheduler

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
 import { appConfig } from "./shared/config/app.config.js";
-
+import "./modules/reminder/reminder.cron.js"
 const PORT = appConfig.port
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
